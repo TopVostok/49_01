add6406805059d10a1874535a4bf20cb356a0637
Kik02 <Golovin.Savelii@yandex.ru>
2026-06-19 15:19:49 +0500

    Разработать DBManager Peshin

---
+++ REST_API_Peshin/Classes/DBManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using REST_API_Peshin.Models;
+
+namespace REST_API_Peshin.Classes
+{
+    public class DBManager : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Dishes> Dishes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DBManager() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1;uid=root;pwd=;database=pr49", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
