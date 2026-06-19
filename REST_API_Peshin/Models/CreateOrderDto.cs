9889ceb0d5e37bb13ba3925849bdc231162ab6df
Kik02 <Golovin.Savelii@yandex.ru>
2026-06-19 15:21:44 +0500

    Пешин Разроботка класса CreateOrderDto

---
+++ REST_API_Peshin/Models/CreateOrderDto.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// DTO для элемента заказа.
+/// </summary>
+public class OrderItemDto
+{
+    /// <summary>Идентификатор блюда.</summary>
+    public int dishId { get; set; }
+    /// <summary>Количество порций.</summary>
+    public int count { get; set; }
+}
+
+/// <summary>
+/// DTO для создания заказа.
+/// </summary>
+public class CreateOrderDto
+{
+    /// <summary>Адрес доставки.</summary>
+    public string address { get; set; }
+    /// <summary>Дата и время доставки.</summary>
+    public string date { get; set; }
+    /// <summary>Список заказываемых блюд.</summary>
+    public List<OrderItemDto> dishes { get; set; }
+}
